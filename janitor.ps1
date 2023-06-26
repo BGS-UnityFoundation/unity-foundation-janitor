345e45c0e57c915fa0f3fab3a8f5dba23c661893
Bruno <b.b.da.costa@gmail.com>
2023-06-26 16:22:50 -0300

    🥅 Add error handling for janitor's parameters

--- janitor.ps1
+++ janitor.ps1
@@ -5,6 +5,16 @@ if ($command -eq "create") {
     $PROJECT_PATH = $args[1]
     $Project_name = $args[2]
 
+    if(!$PROJECT_PATH){
+        Write-Host "Faltando o argumento de caminho do projeto"
+        exit 1
+    }
+
+    if(!$Project_name){
+        Write-Host "Faltando o argumento de nome do projeto"
+        exit 1
+    }
+
     & $PSScriptRoot/src/create-unityfoundation-project.ps1 $PROJECT_PATH $Project_name
     & $PSScriptRoot/src/copy-auxiliaries-files.ps1 $PROJECT_PATH
     & $PSScriptRoot/src/copy-tools.ps1 $PROJECT_PATH
