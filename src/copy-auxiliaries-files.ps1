ad39b460ce481c880ec0a99ccbb30c2adeb64c52
Bruno <b.b.da.costa@gmail.com>
2023-06-28 11:09:23 -0300

    ✨ Add assembly definition creation for each folder in a UnityFoundation module

--- src/copy-auxiliaries-files.ps1
+++ src/copy-auxiliaries-files.ps1
@@ -6,3 +6,5 @@ Copy-Item "$Resource_folder/.gitignore" -Destination $PROJECT_PATH | Out-Null
 Copy-Item "$Resource_folder/.vsconfig" -Destination $PROJECT_PATH | Out-Null
 Copy-Item "$Resource_folder/.editorconfig" -Destination $PROJECT_PATH | Out-Null
 Copy-Item "$Resource_folder/README.md" -Destination $PROJECT_PATH | Out-Null
+
+Write-Host "Arquivos auxiliares copiados."
